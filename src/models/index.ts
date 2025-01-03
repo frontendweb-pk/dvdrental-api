c9d30ffb9854db11a84092620b99d9d149dc2338
frontendweb-sg <frontendweb.sg@gmail.com>
2025-01-04 00:06:57 +0530

    update model index

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,23 @@
+/**
+ * This module sets up the associations between the models in the dvdrental-api.
+ *
+ * Imports:
+ * - `sequelize` from the database configuration.
+ * - `Actor`, `Film`, `Language`, and `FilmActor` models.
+ *
+ * Associations:
+ * - `Film` belongs to many `Actor` through `FilmActor` with `film_id` as the foreign key.
+ * - `Actor` belongs to many `Film` through `FilmActor` with `actor_id` as the foreign key.
+ * - `Language` has many `Film` with `language_id` as the foreign key.
+ * - `Film` belongs to `Language` with `language_id` as the foreign key.
+ *
+ * Exports:
+ * - `sequelize`
+ * - `Actor`
+ * - `Film`
+ * - `Language`
+ * - `FilmActor`
+ */
 import { sequelize } from "../config/db";
 import { Actor } from "./actor";
 import { Film } from "./film";
@@ -6,6 +26,8 @@ import { FilmActor } from "./film-actor";
 
 Film.belongsToMany(Actor, { through: FilmActor, foreignKey: "film_id" });
 Actor.belongsToMany(Film, { through: FilmActor, foreignKey: "actor_id" });
+
 Language.hasMany(Film, { foreignKey: "language_id" });
 Film.belongsTo(Language, { foreignKey: "language_id" });
+
 export { sequelize, Actor, Film, Language, FilmActor };
